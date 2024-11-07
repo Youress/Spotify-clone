eb34294a72e9197a7a5fa1c5eb98c1223f75611f
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-11-07 14:27:30 +0100

    Create SongItem component for song

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -2,7 +2,7 @@ import { Song } from "@/types";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-const getSong = async (): Promise<Song[]> => {
+const getSongs = async (): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies,
   });
@@ -16,4 +16,4 @@ const getSong = async (): Promise<Song[]> => {
     return data || [];
 };
 
-export default getSong
+export default getSongs
