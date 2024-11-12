3834fbc333ed0556988ad1701e86604092b269b0
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-11-12 13:39:39 +0100

    Implement serach by char Ilike

--- actions/getSongByTitle.ts
+++ actions/getSongByTitle.ts
@@ -15,7 +15,7 @@ const getSongByTitle = async (title: string): Promise<Song[]> => {
   const { data, error } = await supabase
     .from("songs")
     .select("*")
-    .ilike("title", `${title}`)
+    .ilike("title", `%${title}%`)
     .order("created_at", { ascending: false });
   if (error) {
     console.log(error);
