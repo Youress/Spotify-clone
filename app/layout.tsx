6de50fa69dfcbb2b50d9e76864b13554c0b6691e
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-11-13 12:34:52 +0100

    add Player in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongByUserId from "@/actions/getSongByUserId";
+import Player from "@/components/Player";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,6 +41,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider/>
             <SideBar songs={userSongs}>{children}</SideBar>
+            <Player/>
           </UserProvider>
         </SupabaseProvider>
       </body>
