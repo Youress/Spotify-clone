f99fa48fcd8bf102fd22356dbe69033ab21047eb
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-11-15 17:09:34 +0100

    add and init stripe

---
+++ libs/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from 'stripe'
+
+export const stripe = new Stripe(
+    process.env.STRIPE_SECRET_KEY ?? ''
+,{
+    apiVersion:'2024-10-28.acacia',
+    appInfo : {
+        name :'Spotify clone',
+        version :'0.1.0'
+    }
+}
+)
